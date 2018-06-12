bf737f80246ead71f2d3687265e4859258c54355
v2ray-shell <v2ray-shell>
2018-06-13 05:55:03 +0800

    fix codegen.py

--- src/bridgehouse/editMap/router/codegen.py
+++ src/bridgehouse/editMap/router/codegen.py
@@ -1,8 +1,7 @@
 import os
-import sys
 import grpc_tools.protoc as proto # python -m pip install grpcio-tools
 
-protoFiles = [x for x in os.listdir() if x.endswith(".proto")]
+protoFiles = [x for x in os.listdir() if x.endswith(".proto") and os.path.isfile(x)]
 
 for p in protoFiles:
     proto.main("--proto_path=./ --python_out=./ --grpc_python_out=./ {}".format(p).split())
